61eecb73c722e5fa48b854695c2cf8c8cc31e789
Johannes Konings <johannes.konings@outlook.com>
2025-12-06 09:53:31 -0800

    add GSIs and LogGroup

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,33 @@
 import { App, Stack, StackProps, aws_dynamodb as dynamodb } from 'aws-cdk-lib';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
 
 export class CdkNotfifierFeatureStackExample extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    new dynamodb.Table(this, 'Table', {
+    const table = new dynamodb.Table(this, 'Table', {
       tableName: `Table-${branchName}`,
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    new LogGroup(this, 'LogGroup', {
+      logGroupName: 'alreadyexisting',
+    });
+
+    // Add GSI 1
+    table.addGlobalSecondaryIndex({
+      indexName: 'GSI1',
+      partitionKey: { name: 'gsi1pk', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'gsi1sk', type: dynamodb.AttributeType.STRING },
+    });
+
+    // Add GSI 2
+    table.addGlobalSecondaryIndex({
+      indexName: 'GSI2',
+      partitionKey: { name: 'gsi2pk', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'gsi2sk', type: dynamodb.AttributeType.STRING },
     });
   }
 }
